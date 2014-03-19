bf4af1ffbeba844594435cbe4644893081080bb2
Miguel Angel Torres <miguelangel.torres@outlook.com>
2014-03-19 22:25:20 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html manifest="cache.manifest">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/stylecss.css"/>
+        <title>Probando APP OFFLINE</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div class="container">
+            <div class="header">
+                <img src="images/rode.jpg"/>
+            </div>
+            <div class="texto">
+                <h1>Texto de prueba</h1>
+                Lorem isump text. Lorem isump text.Lorem isump text.Lorem isump text.Lorem isump text.<br/>
+            </div>Lorem isump text. Lorem isump text.Lorem isump text.Lorem isump text.Lorem isump text.
+            Lorem isump text. Lorem isump text.Lorem isump text.Lorem isump text.Lorem isump text.<br/>
+            Lorem isump text. Lorem isump text.Lorem isump text.Lorem isump text.Lorem isump text.
+            Lorem isump text. Lorem isump text.Lorem isump text.Lorem isump text.Lorem isump text.
+            Lorem isump text. Lorem isump text.Lorem isump text.Lorem isump text.Lorem isump text.<br/>
+        </div>
+    </body>
+</html>
